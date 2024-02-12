9740cf87ff45b861867da99254d83abe117131dc
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-02-12 20:58:36 +0530

    commit

--- Step-3_Array-solutions/Easy-problem/test.cpp
+++ Step-3_Array-solutions/Easy-problem/test.cpp
@@ -1,119 +1,46 @@
-#include <bits/stdc++.h>
-
+#include<iostream>
+#include"functions.cpp"
 using namespace std;
 
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
-
-/*
- * Complete the 'compareTriplets' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY a
- *  2. INTEGER_ARRAY b
- */
-
-vector<int> compareTriplets(vector<int> a, vector<int> b) {
-    vector<int> point_a;
-    vector<int> point_b;
-    for(int i=0;i<a.size();i++){
-        if (a[i]>b[i]) {
-           point_a.push_back(1);
+ void sort012(int a[], int n)
+    {
+        // code here 
+    int low=0,mid=0;
+        int high=n-1;
+        while(mid<=high)
+        {
+            if(a[mid]==0)
+            {
+                swap(a[low],a[mid]);
+                low++;
+                mid++;
+            }
+            else if(a[mid]==1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(a[mid],a[high]);
+                high--;
+            }
         }
-        if (b[i]>a[i]) {
-            point_b.push_back(1);
-        }
-    }
-    for (int i=0; i<a.size(); i++) {
-     cout<<point_a[i]<<" "<<point_b[i];
-    }
-
 }
 
-int main()
+main(){
+int n;
+cin>>n;
+int arr[n];
+for (int i = 0; i < n; i++)
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string a_temp_temp;
-    getline(cin, a_temp_temp);
-
-    vector<string> a_temp = split(rtrim(a_temp_temp));
-
-    vector<int> a(3);
-
-    for (int i = 0; i < 3; i++) {
-        int a_item = stoi(a_temp[i]);
-
-        a[i] = a_item;
-    }
-
-    string b_temp_temp;
-    getline(cin, b_temp_temp);
-
-    vector<string> b_temp = split(rtrim(b_temp_temp));
-
-    vector<int> b(3);
-
-    for (int i = 0; i < 3; i++) {
-        int b_item = stoi(b_temp[i]);
-
-        b[i] = b_item;
-    }
-
-    vector<int> result = compareTriplets(a, b);
-
-    for (size_t i = 0; i < result.size(); i++) {
-        fout << result[i];
-
-        if (i != result.size() - 1) {
-            fout << " ";
-        }
-    }
-
-    fout << "\n";
-
-    fout.close();
-
-    return 0;
+        cin>>arr[i];
+}
+sort012(arr,n);
+for (int i = 0; i < n; i++)
+{
+    cout<<arr[i]<<" ";
 }
 
-string ltrim(const string &str) {
-    string s(str);
 
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
+return 0;
 }
