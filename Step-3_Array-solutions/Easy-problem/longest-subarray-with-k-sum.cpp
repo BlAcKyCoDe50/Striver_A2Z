03e433356e693e54c86143b180fbd9bd972938f0
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-02-15 21:17:06 +0530

    longest-subarray

---
+++ Step-3_Array-solutions/Easy-problem/longest-subarray-with-k-sum.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+#include <bits/stdc++.h>
+#include "functions.cpp"
+using namespace std;
+
+//Brute force approach
+
+// int longestSubarray(vector<int>&arr,int summm){
+//    int len = 0;
+//     for (int i = 0; i < arr.size(); i++) {
+//         int sum = 0; // Initialize sum for each subarray
+//         for (int j = i; j < arr.size(); j++) {
+//             sum += arr[j];
+//             if (sum == summm) {
+//                 len = max(len, j - i + 1);
+//             }
+//         }
+//     }
+//     return len;
+// }
+
+//Better approach
+
+int longestSubarray(vector<int> arr,long k){
+    map<long long,int> presum;
+    long long sum=0;
+    int maxLen=0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        sum=sum+arr[i];
+        if (sum==k)
+        {
+         maxLen=i+1;   
+        }
+        int rem=sum-k;
+        if(presum.find(rem)!=presum.end()){
+            int len=i-presum[rem];
+            maxLen=max(maxLen,len);
+        }
+        presum[sum]=i;
+    }
+    return maxLen;   
+}
+
+
+main(){
+int n;
+cin>>n;
+
+vector<int> arr(n);
+array_insertion(arr,n);
+cout<<"Subarray is: "<<longestSubarray(arr,6);
+
+return 0;
+}
