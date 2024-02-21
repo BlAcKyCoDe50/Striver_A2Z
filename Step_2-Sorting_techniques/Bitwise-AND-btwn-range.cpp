543d0c32d3b0df75054894985f881282e583c714
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-02-22 00:10:10 +0530

    bitwise OR btwn range

---
+++ Step_2-Sorting_techniques/Bitwise-AND-btwn-range.cpp
@@ -0,0 +1,35 @@
+// Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
+
+// Example 1:
+
+// Input: left = 5, right = 7
+// Output: 4
+// Example 2:
+
+// Input: left = 0, right = 0
+// Output: 0
+// Example 3:
+
+// Input: left = 1, right = 2147483647
+// Output: 0
+
+#include<iostream>
+using namespace std;
+
+int rangeBitwiseAnd(int left, int right) {
+        int cnt = 0;
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            cnt++;
+        }
+        return (left << cnt);
+    }
+
+main(){
+int n,m;
+cin>>n>>m;
+cout<<rangeBitwiseAnd(n,m);
+
+return 0;
+}
