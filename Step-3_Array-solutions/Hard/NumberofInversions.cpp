9580d33b96b0e06a4747c6abf83a3baf22845de1
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-04-16 11:25:30 +0530

    number of inversions using merge sort

---
+++ Step-3_Array-solutions/Hard/NumberofInversions.cpp
@@ -0,0 +1,78 @@
+/*
+There is an integer array ‘A’ of size ‘N’.
+
+Number of inversions in an array can be defined as the number of pairs of ‘i’, ‘j’ such that ‘i’ < ‘j’ and ‘A[i]’ > ‘A[j]’.
+
+You must return the number of inversions in the array.
+
+For example,
+Input:
+A = [5, 3, 2, 1, 4], N = 5
+Output:
+7
+Explanation: 
+The pairs satisfying the condition for inversion are (1, 2), (1, 3), (1, 4), (1, 5), (2, 3), (2, 4), and (3, 4). 
+The number of inversions in the array is 7.
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int cnt=0;
+int merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1;   // starting index of right half of arr
+
+    //storing elements in the temporary array in a sorted manner//
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        //right is smaller
+        else {
+            temp.push_back(arr[right]);
+             cnt=cnt+(mid-left+1);
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while(left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high) {
+    if (low >= high) return;
+    int mid = (low + high) / 2 ;
+    mergeSort(arr, low, mid);  // left half
+    mergeSort(arr, mid + 1, high); // right half
+    merge(arr, low, mid, high);  // merging sorted halves
+}
+
+
+
+int numberOfInversions(vector<int>&arr, int n) {
+    // Write your code here.
+    mergeSort(arr,0,n-1);
+    return cnt;
+    
+    }
