76a064120347d84254d7453f3ee5d2066c7ff994
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-05-20 00:30:12 +0530

    testing question

--- Step-3_Array-solutions/Easy-problem/test.cpp
+++ Step-3_Array-solutions/Easy-problem/test.cpp
@@ -1,33 +1,16 @@
 #include<bits/stdc++.h>
-// #include<functional>
-#include "functions.cpp"
 using namespace std;
 
+int main(){
 
-int fact(int n){
-    if (n == 0 || n == 1) {
-        return 1;
-    } else {
-        return n * fact(n - 1);
-    }   
+int n;int m;int k;
+cin>>n;
+cin>>m;
+cin>>k;
+int ans=(int) (pow(n,m));
+while(ans>=10){
+int ans=ans/10;
 }
-
-int main() {
-    // vector<int> nums = {1, 4, 5, 2, 3, 4, 2, 3, 4, 2};
-    // int target = 2;
-    // unordered_map<int,int> mpp;
-    // for(auto it: nums){
-    //     mpp[it]++;  
-    // }
-    // cout<< mpp[target];
-
-    int n=4;
-    int r=2;
-    int nfact=fact(n);
-    int rfact=fact(r);
-
-    int fin=nfact/(rfact*(fact(n-r)));
-    cout<<fin;
-
+cout << ans;
+return 0;
 }
-
