d213727638868a4d6ba33feb317032d57b621bbb
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-05-31 04:29:03 +0530

    testing code file

--- Step-4-Binary_Search_problems/BS-on-Answers/testing.cpp
+++ Step-4-Binary_Search_problems/BS-on-Answers/testing.cpp
@@ -1,38 +1,23 @@
-// #include<iostream>
+#include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 main(){
-// int n;
-// cin>>n;
-vector<int> arr={1,2,3,4,5}; //1
-vector<int> arr1={6,1,2,9,0};
-vector<int>ans;
-// sort(arr.begin(),arr.end());
-// sort(arr1.begin(),arr1.end());
-// for (int i = 0; i < arr.size(); i++)
-// {
-//     bool flag=true;
-//     for (int j = 0; j < arr1.size(); j++)
-//     {
-//         if(arr[i]==arr1[j]) { flag=true; break;}
-//     }
-//     if(flag==false) ans.push_back(arr[i]);
-// }
-int i=0;int j=0;
-while(i<arr.size() && j<arr.size())
-{
-    if(arr[i]==arr[j])
+    vector<string> arr=
+    vector<string> arr1;
+    cout<<"1";
+    int j=0;
+    for (int i = arr.size(); i>=0; i--)
     {
-        i++;j=0;
-        // cout<<"current i: "<<arr[i]<<endl;
-        
+        cout<<"1";
+        arr1[j]=arr[i];
+        j++;
     }
-     if(j==arr.size()-1) {cout<<"in if"<<endl;ans.push_back(arr[i]);}
-    else j++;
-}
-for (int i = 0; i < ans.size(); i++)
-{
-    cout<<ans[i]<<" ";
-}
+    arr=arr1;
+    
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout<<arr[i];
+    }
+    
 return 0;
 }
