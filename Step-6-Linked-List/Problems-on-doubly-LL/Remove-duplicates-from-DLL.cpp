d37fc0f152c5e351e493cf28964958bdb220ce8d
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-06-28 09:46:51 +0530

    remove duplicates from DLL

---
+++ Step-6-Linked-List/Problems-on-doubly-LL/Remove-duplicates-from-DLL.cpp
@@ -0,0 +1,39 @@
+  #include<iostream>
+  #include<bits/stdc++.h>
+  using namespace std;
+  class Node{
+  public:
+  int data; Node *next; Node*prev;
+  Node(int data1) {
+  data=data1;
+   next=nullptr;
+  } 
+  };
+
+
+    Node * removeDuplicates(struct Node *head)
+    {
+        // Your code here
+        Node *temp = head;
+        
+        while(temp!=nullptr && temp->next!=nullptr)
+        {
+            Node *nextnode = temp->next;
+            while(nextnode!=nullptr && temp->data == nextnode->data)
+            {
+                    nextnode = nextnode -> next;
+            }
+            temp->next=nextnode;
+            if(nextnode) nextnode->prev= temp;
+            temp = temp->next;
+        }
+        return head;
+    }
+
+main(){
+  Node*head=new Node(1);
+  head->next=new Node(2);
+  head->next->next=new Node(3);
+
+  return 0;
+  }
