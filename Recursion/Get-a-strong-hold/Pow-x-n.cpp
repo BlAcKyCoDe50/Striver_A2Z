d67b1e979b1b622bea90456703ceea3a5ae65d26
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-07-02 23:06:47 +0530

    Recursion problems

---
+++ Recursion/Get-a-strong-hold/Pow-x-n.cpp
@@ -0,0 +1,23 @@
+   #include<iostream>
+   #include<bits/stdc++.h>
+   using namespace std;
+
+ double solve(double x,long n)
+    {
+        if(n==0) return 1;
+        if(n<0)
+        {
+           return  solve(1/x,-n);
+        }
+        if(n%2==0)  return solve(x*x,n/2);
+        return x*solve(x*x,(n-1)/2);
+    }
+    
+    double myPow(double x, int n) 
+    {
+        return solve(x,(long)n);
+    }
+
+main(){
+ return 0;
+ }
