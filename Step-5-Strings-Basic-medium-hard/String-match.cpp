f95c0d1328afe22daf54a204c4f225f113a25fd2
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-07-10 22:54:43 +0530

    string-match

---
+++ Step-5-Strings-Basic-medium-hard/String-match.cpp
@@ -0,0 +1,22 @@
+   #include<iostream>
+   #include<bits/stdc++.h>
+   using namespace std;
+
+   vector<int>match(vector<string> arr,vector<string> arr2)
+   {
+        vector<int>cnt;
+        map<string,int>mp;
+
+        for(int i=0;i<arr.size();i++)
+        {
+            mp[arr2[i]]++;
+        }
+
+
+   }
+main(){
+ 
+
+
+ return 0;
+ }
