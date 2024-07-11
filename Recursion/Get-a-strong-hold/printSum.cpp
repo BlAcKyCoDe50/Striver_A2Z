1051152f1557bae1e7a2f18713ea1b30e586825e
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-07-12 01:07:24 +0530

    print the pair of given sum

---
+++ Recursion/Get-a-strong-hold/printSum.cpp
@@ -0,0 +1,43 @@
+/*
+
+we have to print the subsets where the sum is equal to the given sum
+
+*/
+
+   #include<iostream>
+   #include<bits/stdc++.h>
+   using namespace std;
+
+    void printS(int i,vector<int> &ds,int s,int sum,int arr[],int n)
+    {
+        
+        if(i==n)
+        {
+            if(s==sum)
+            {
+                for(auto it:ds) cout<<it<<" ";
+                cout<<endl;
+            }
+            return;
+        }
+
+        ds.push_back(arr[i]); //take
+        s+=arr[i]; 
+        printS(i+1,ds,s,sum,arr,n);
+
+        s-=arr[i];
+        ds.pop_back(); //not take
+
+        printS(i+1,ds,s,sum,arr,n);
+
+    }
+
+main(){
+ int n=3;
+ int arr[] = {1,2,1};
+ int sum =2;
+ vector<int>ds;
+ printS(0,ds,0,sum,arr,n);
+
+ return 0;
+ }
